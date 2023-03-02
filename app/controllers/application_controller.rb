ab1e67f42618b66a9f27391a860a1c87117bef9f
Lorena Cueva <lorenacueva@gmail.com>
2023-03-01 22:33:00 -0800

    commit tree 3b4137367fd9a00d9c7ff946008f76945f4b7e74

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_response
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+def record_invalid_response e
+  render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+end
+
+def not_found_response
+  render json: {errors: "Record Not found"}, status: :not_found
+end
+
+def authorize 
+  return render json: {errors: ["Not Authorized"]}, status: :unauthorized unless session.include? :user_id
+end 
 
 end
